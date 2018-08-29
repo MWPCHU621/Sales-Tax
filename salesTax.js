67f93bb88175e312956811f98024492582d21f91
Patrick Chu <mingweipatrickc@gmail.com>
2018-08-29 19:01:08 +0000

    added empty calculate method as well as testcase

--- salesTax.js
+++ salesTax.js
@@ -21,3 +21,23 @@ var companySalesData = [
     sales: [ 500, 100 ]
   }
 ];
+
+
+function calculateSalesTax(salesData, taxRates) {
+  // Implement your code here
+}
+
+var results = calculateSalesTax(companySalesData, salesTaxRates);
+
+/* Expected Results:
+{
+  Telus: {
+    totalSales: 1300
+    totalTaxes: 144
+  },
+  Bombardier: {
+    totalSales: 800,
+    totalTaxes: 40
+  }
+}
+*/
